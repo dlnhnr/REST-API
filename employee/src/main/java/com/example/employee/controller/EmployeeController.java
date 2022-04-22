081ff34b147826d8cbff60f491f398e78e18ea85
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-04-22 17:51:19 -0400

    thyme

--- employee/src/main/java/com/example/employee/controller/EmployeeController.java
+++ employee/src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.employee.controller;
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,12 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService empService;
 	
+	private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+	
 	@RequestMapping(value="/employees", method=RequestMethod.POST)
 	public Employee createEmployee(@RequestBody Employee emp) {
-	    return empService.createEmployee(emp);
+		log.info("Post a new employee");
+		return empService.createEmployee(emp);
 	}
 
 	@RequestMapping(value="/employees", method=RequestMethod.GET)
@@ -28,8 +33,22 @@ public class EmployeeController {
 	    return empService.getEmployees();
 	}
 	
+	@RequestMapping(value="/employees/template", method=RequestMethod.GET)
+	public String templateEmployees(Model model) {
+	   	
+		model.addAttribute("employees", empService.getEmployees());
+		return "employees";
+		//return empService.Employees();
+	}
+	
 	@RequestMapping(value="/employees/{empId}", method=RequestMethod.PUT)
 	public Employee updateEmployee(@PathVariable(value = "empId") Long id, @RequestBody Employee empDetails) {
+	    if(id > 2000) {
+	    	log.warn("The user is trying to access a large id number");
+	    	log.debug("Running this");
+	    	log.error("Checking how logging custom error works");
+	    }
+	    
 	    return empService.updateEmployee(id, empDetails);
 	}
 	
